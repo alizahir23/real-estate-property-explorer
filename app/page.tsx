2e616f12d2c6abae5252d5a4b11a01785ffa60fc
Ali Zahir <alizahiruddin@gmail.com>
2025-01-20 11:41:13 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
+import { Metadata } from "next";
 import Navbar from "./components/Navbar";
 import SearchFilterBar from "./components/SearchFilterBar";
 import PropertyExplorer from "./components/property_explorer/PropertyExplorer";
 
-export default function Home({
+export const metadata: Metadata = {
+  title: "Real Estate Property Explorer",
+  description: "Explore properties with interactive map and list views",
+};
+
+export default async function Home({
   searchParams,
 }: {
   searchParams: { query?: string };
 }) {
-  const query = searchParams.query;
+  const query = (await searchParams).query || "";
 
   return (
     <>
