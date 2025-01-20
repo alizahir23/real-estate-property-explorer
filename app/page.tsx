9fa73492bc14e15a08e7cf22984b821d2f50f308
Ali Zahir <alizahiruddin@gmail.com>
2025-01-20 12:33:56 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,26 @@ import { Metadata } from "next";
 import Navbar from "./components/Navbar";
 import PropertyExplorer from "./components/property_explorer/PropertyExplorer";
 import SearchFilterBar from "./components/SearchFilterBar";
+import { use } from "react";
 
 export const metadata: Metadata = {
   title: "Real Estate Property Explorer",
   description: "Explore properties with interactive map and list views",
 };
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined };
-}) {
+type Props = {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+export default function Page({ searchParams }: Props) {
   // Extract the query/filters from searchParams
-  const query = (await searchParams).filters as string | undefined;
+  const query = use(searchParams).query ?? "";
 
   return (
     <>
       <Navbar />
-      <SearchFilterBar query={query} />
-      <PropertyExplorer query={query} />
+      <SearchFilterBar query={String(query)} />
+      <PropertyExplorer query={String(query)} />
     </>
   );
 }
